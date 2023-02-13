a4a2202cece801c5dac15ce936611fd60cb8e48a
Deta1l <57856724+Deta1l@users.noreply.github.com>
2023-02-13 18:31:34 +0300

    1

--- otchet.py
+++ otchet.py
@@ -1,1 +1,14 @@
 from openpyxl import load_workbook
+
+outputf = "otchet.xlsx"
+
+wb = load_workbook(outputf)
+ws = wb['Лист1']
+
+ws['A6'] = 'Отчет стыковок'
+
+
+
+wb.save(outputf)
+wb.close()
+print("otchet gotov")
