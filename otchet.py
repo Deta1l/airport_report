4f03b1926f30bf933c911fdce367303ccae1dc62
Deta1l <57856724+Deta1l@users.noreply.github.com>
2023-02-13 23:06:36 +0300

    1f

--- otchet.py
+++ otchet.py
@@ -1,7 +1,11 @@
 from openpyxl.styles import Border, Side, PatternFill, Font, GradientFill, Alignment
-from openpyxl import Workbook
+from openpyxl import Workbook, open
 
 wb = Workbook()
+
+schedule_xlsx = open('data\Schedule_LT.xlsx', read_only=True) 
+print(schedule_xlsx)
+
 ws = wb.active
 
 ws.merge_cells('A1:B4')
