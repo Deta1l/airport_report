92931b3307f47e3dc3245fad0c49f4434d12d671
Deta1l <57856724+Deta1l@users.noreply.github.com>
2023-02-13 21:19:18 +0300

    3

--- otchet.py
+++ otchet.py
@@ -1,19 +1,43 @@
-from openpyxl import load_workbook
+from openpyxl.styles import Border, Side, PatternFill, Font, GradientFill, Alignment
+from openpyxl import Workbook
 
-outputf = "otchet.xlsx"
+wb = Workbook()
+ws = wb.active
 
-wb = load_workbook(outputf)
-ws = wb['Лист1']
+ws.merge_cells('A1:B4')
+start = ws['A1']
+start.value = "OnD"
+start.alignment = Alignment(horizontal="center", vertical="center")
 
-
-
-
-ws.merge_cells('B2:F4')
-ws['b2'] = 'Отчет стыковок'
-
-
+thin = Side(border_style="thin", color="000000")
 
 
-wb.save(outputf)
+ws.merge_cells('A5:B9')
+ws.merge_cells('C5:C9')
+
+
+ws.merge_cells('C2:J2')
+s7 = ws['C2']
+s7.value = "S7"
+s7.alignment = Alignment(horizontal="center", vertical="center")
+
+ws.merge_cells('C3:C4')
+ws['C3'] = 'Город стыковки'
+ws.merge_cells('D3:D4')
+ws['D3'] = '№ рейсов'
+ws.merge_cells('E3:E4')
+ws['E3'] = 'период'
+ws.merge_cells('F3:F4')
+ws['F3'] = 'дни недели'
+ws.merge_cells('G3:G4')
+ws['G3'] = 'вылет'
+ws.merge_cells('H3:H4')
+ws['H3'] = 'прилет'
+ws.merge_cells('I3:I4')
+ws['I3'] = 'время стык'
+ws.merge_cells('J3:J4')
+ws['J3'] = 'время полета'
+
+wb.save('otchet.xlsx')
 wb.close()
-print("otchet gotov")
+print("отчет готов!")
