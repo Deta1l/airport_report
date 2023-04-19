ad7e0797ecea53345e9b41f7fd773b5fbf922196
Deta1l <57856724+Deta1l@users.noreply.github.com>
2023-04-19 15:04:37 +0300

    3

--- algo.py
+++ algo.py
@@ -4,16 +4,20 @@ from openpyxl import load_workbook, open
 
 #schedule_xlsx = open('data\Schedule_LT.xlsx', read_only=True) 
 
-
+mct_gruz = 'data\MCT_груз.xlsx'
 schedule_xlsx  = 'data\Schedule_LT.xlsx'
 
 wb = load_workbook(schedule_xlsx)
+mct = load_workbook(mct_gruz)
 
 answer = {}
 city = []
 
 ws = wb.active
 m_row = ws.max_row
+
+mct_ws = mct.active
+mct_row = mct_ws.max_row
 
 for i in range(2, m_row + 1):
     cell_obj = ws.cell(row = i, column = 2)
@@ -36,12 +40,14 @@ for i in range(2, m_row + 1):
 
 def dfs_paths(graph, n, start, goal, path=[], count=0):
     path = path + [start]
+
     if start == goal and len(path) <= n+2:
         print(path)
         return
     
     for next_node in graph[start]:
         if next_node not in path:
+            #проверка на время + время стоянки
             dfs_paths(graph, n, next_node, goal, path, count+1)
 
 
@@ -55,7 +61,7 @@ start = 'KQT'
 end = 'OSS'
 n = 2
 
-#dfs_paths(answer, n , start, end, [], 0)
+dfs_paths(answer, n , start, end, [], 0)
 
 print("finish")
 
@@ -68,9 +74,8 @@ for i in range(2, m_row + 1):
     cell_obj1 = ws.cell(row = i, column = 5)
     time_1 = dt.strptime(str(cell_obj.value),"%H:%M:%S")
     time_2 = dt.strptime(str(cell_obj1.value),"%H:%M:%S")
-
     time_interval = time_2 - time_1
-    print(time_interval.strftime("%H:%M:%S"))
+    #print(time_interval)
 
 
 wb.close()
