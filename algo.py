9a22f99ff7533a38baecc0380cf8eecf3db0650e
Deta1l <57856724+Deta1l@users.noreply.github.com>
2023-04-20 15:05:04 +0300

    near to the end !

--- algo.py
+++ algo.py
@@ -5,36 +5,37 @@ from openpyxl import load_workbook, open
 #schedule_xlsx = open('data\Schedule_LT.xlsx', read_only=True) 
 
 mct_gruz = 'data\MCT_груз.xlsx'
-schedule_xlsx  = 'data\Schedule_LT.xlsx'
+schedule_xlsx1  = 'data\Schedule_LT.xlsx'
 
-wb = load_workbook(schedule_xlsx)
+wb1 = load_workbook(schedule_xlsx1)
 mct = load_workbook(mct_gruz)
 
 answer = {}
 city = []
+route = []
 
-ws = wb.active
-m_row = ws.max_row
+ws1 = wb1.active
+m_row = ws1.max_row
 
 mct_ws = mct.active
 mct_row = mct_ws.max_row
 
 for i in range(2, m_row + 1):
-    cell_obj = ws.cell(row = i, column = 2)
+    cell_obj = ws1.cell(row = i, column = 2)
     if cell_obj.value not in city:
         city.append(cell_obj.value)
 
         for a in range(2, m_row + 1):
-            cell_obj1 = ws.cell(row = a, column = 2)
+            cell_obj1 = ws1.cell(row = a, column = 2)
             if cell_obj1.value == cell_obj.value:
-                g1 = ws.cell(row = a, column = 4)
+                g1 = ws1.cell(row = a, column = 4)
                 answer[cell_obj.value]=[g1.value]
                 break
         
         for a in range(2, m_row + 1):
-            cell_obj1 = ws.cell(row = a, column = 2)
+            cell_obj1 = ws1.cell(row = a, column = 2)
             if cell_obj1.value == cell_obj.value:
-                g1 = ws.cell(row = a, column = 4)
+                g1 = ws1.cell(row = a, column = 4)
                 answer[cell_obj.value].append(g1.value)
 
 
@@ -42,7 +43,7 @@ def dfs_paths(graph, n, start, goal, path=[], count=0):
     path = path + [start]
 
     if start == goal and len(path) <= n+2:
-        print(path)
+        route.append(path)
         return
     
     for next_node in graph[start]:
@@ -57,15 +58,16 @@ graph = {'a':['b'],
     
 }
 
-start = 'KQT'
-end = 'OSS'
+start = 'OVB'
+end = 'EVN'
 n = 2
 
 dfs_paths(answer, n , start, end, [], 0)
+print(route)
 
 print("finish")
 
-print("-----------------------------")
+'''
 
 from datetime import datetime as dt 
 
@@ -76,6 +78,9 @@ for i in range(2, m_row + 1):
     time_2 = dt.strptime(str(cell_obj1.value),"%H:%M:%S")
     time_interval = time_2 - time_1
     #print(time_interval)
+'''
+
+#--------------------------------вывод
 
 
-wb.close()
+wb1.close()
--- otchet.py
+++ otchet.py
@@ -1,5 +1,5 @@
 from openpyxl.styles import Border, Side, PatternFill, Font, GradientFill, Alignment
-from openpyxl import Workbook, open
+from openpyxl import Workbook, load_workbook, open
 
 wb = Workbook()
 
@@ -17,7 +17,7 @@ start.alignment = Alignment(horizontal="center", vertical="center")
 #от до
 ws.merge_cells('A5:B9')
 ws['A5'].alignment = Alignment(horizontal="center", vertical="center")
-ws['A5'] = input("введине направление ")
+#ws['A5'] = input("введине направление ")
 
 #стыковка
 ws.merge_cells('C5:C9')
@@ -52,6 +52,133 @@ ws.column_dimensions['I'].width = 15
 ws.merge_cells('J3:J4')
 ws['J3'] = 'время полета'
 ws.column_dimensions['J'].width = 15
+
+
+#schedule_xlsx = open('data\Schedule_LT.xlsx', read_only=True) 
+
+mct_gruz = 'data\MCT_груз.xlsx'
+schedule_xlsx1  = 'data\Schedule_LT.xlsx'
+
+wb1 = load_workbook(schedule_xlsx1)
+mct = load_workbook(mct_gruz)
+
+answer = {}
+city = []
+route = []
+
+ws1 = wb1.active
+m_row = ws1.max_row
+
+mct_ws = mct.active
+mct_row = mct_ws.max_row
+
+for i in range(2, m_row + 1):
+    cell_obj = ws1.cell(row = i, column = 2)
+    if cell_obj.value not in city:
+        city.append(cell_obj.value)
+
+        for a in range(2, m_row + 1):
+            cell_obj1 = ws1.cell(row = a, column = 2)
+            if cell_obj1.value == cell_obj.value:
+                g1 = ws1.cell(row = a, column = 4)
+                answer[cell_obj.value]=[g1.value]
+                break
+        
+        for a in range(2, m_row + 1):
+            cell_obj1 = ws1.cell(row = a, column = 2)
+            if cell_obj1.value == cell_obj.value:
+                g1 = ws1.cell(row = a, column = 4)
+                answer[cell_obj.value].append(g1.value)
+
+
+def dfs_paths(graph, n, start, goal, path=[], count=0):
+    path = path + [start]
+
+    if start == goal and len(path) <= n+2:
+        route.append(path)
+        return
+    
+    for next_node in graph[start]:
+        if next_node not in path:
+            #проверка на время + время стоянки
+            dfs_paths(graph, n, next_node, goal, path, count+1)
+
+
+start = 'OVB'
+end = 'EVN'
+n = 2
+
+dfs_paths(answer, n , start, end, [], 0)
+print(route)
+
+print("finish")
+
+
+#--------------------------------вывод
+
+lines_number = 5
+
+for one_route in route:
+ 
+    if len(one_route) == 2:
+        ws['C5'].alignment = Alignment(horizontal="center", vertical="center")
+        ws['C5'] = 'Direct Flight'
+        start_airport = one_route[0]
+        end_airport = one_route[1]
+        for i in range(2, m_row + 1):
+            cell_obj = ws1.cell(row = i, column = 2)
+            cell_obj1 = ws1.cell(row = i, column = 4)
+            if (start_airport == cell_obj.value) and (end_airport == cell_obj1.value):
+                flight_number = ws1.cell(row = i, column = 1) #ВЫВОД
+                ws.cell(row = lines_number, column = 4, value = flight_number.value)
+                date1 = ws1.cell(row = i, column = 6)
+                date1 = str(date1.value)
+                flite_dates, flight_reg = date1.split()  #2ВЫВОД
+                ws.cell(row = lines_number, column = 5, value = flite_dates)
+                ws.cell(row = lines_number, column = 6, value = flight_reg)
+            
+                dep = flight_nomber = ws1.cell(row = i, column = 3) #ВЫВОД
+                arr = flight_nomber = ws1.cell(row = i, column = 5) #ВЫВОД
+
+                ws.cell(row = lines_number, column = 7, value = dep.value)
+                ws.cell(row = lines_number, column = 8, value = arr.value)
+                ws.cell(row = lines_number, column = 9, value = 0)
+                lines_number+=1
+                #длительность стыковыки 0, посчитать время полета
+
+                #for col in ws.iter_cols(min_row=5, min_col=4, max_row=5, max_col=10):
+                #    for cell in col:
+
+
+
+
+
+print("finish")
+
+wb1.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 wb.save('otchet.xlsx')
